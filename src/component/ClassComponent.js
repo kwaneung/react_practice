a03288d1645599b1bf594f1ff7653de642fac8de
kwaneung <kwaneung.kim@outlook.com>
2023-03-17 10:02:29 +0900

    함수형, 클래스형 컴포넌트 생성 및 외부참조

---
+++ src/component/ClassComponent.js
@@ -0,0 +1,11 @@
+import {Component, Fragment} from "react";
+
+export default class ClassComponent extends Component{
+    render() {
+        return (
+            <Fragment>
+                <h3>Class Component!</h3>
+            </Fragment>
+        );
+    }
+}
---
+++ src/component/FunctionComponent.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from "react";
+
+const FunctionComponent = () => {
+    return (
+        <Fragment>
+            <h1 style={{border: '1px solid black'}}>Function Component Ref!!</h1>
+        </Fragment>
+    )
+}
+export default FunctionComponent;
